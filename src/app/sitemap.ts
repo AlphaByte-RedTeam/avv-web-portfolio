fb475fa0cd9cff96161d9ae39e2c5f662cda83e1
Andrew Virya Victorio <andrew.avv03@gmail.com>
2026-01-20 13:39:19 +0700

    feat: add robots and sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,38 @@
+import { MetadataRoute } from 'next'
+import { getPayload } from 'payload'
+import config from '@/payload.config'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const payloadConfig = await config
+  const payload = await getPayload({ config: payloadConfig })
+
+  const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL || 'https://example.com'
+
+  const { docs: posts } = await payload.find({
+    collection: 'blog',
+    limit: 1000,
+  })
+
+  const postsUrls = posts.map((post) => {
+    return {
+      url: `${baseUrl}/blog/${post.slug}`,
+      lastModified: new Date(post.lastUpdated || post.date),
+    }
+  })
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/resume`,
+      lastModified: new Date(),
+    },
+    ...postsUrls,
+  ]
+}
