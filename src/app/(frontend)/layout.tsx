3525aab2438b4c094a3d2189deea903540cc1523
Andrew Virya Victorio <andrew.avv03@gmail.com>
2026-01-25 10:31:12 +0700

    chore: add suppress hydration warning property

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID as string} />
       <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID as string} />
       <body
